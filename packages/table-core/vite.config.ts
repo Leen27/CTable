74d70eb3b722ee3e580b5b4a06df623bc194053a
Leen27 <m13840918032@163.com>
2025-11-13 16:52:05 +0800

    remove(table-core): remove example

--- packages/table-core/vite.config.ts
+++ packages/table-core/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
+import dts from 'vite-plugin-dts'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      tsconfigPath: 'tsconfig.json',
+      exclude: ['src/**/*.spec.ts']
+    })
+  ],
   build: {
     outDir: 'dist',
     lib: {
