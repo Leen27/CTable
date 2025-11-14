29d3c602b451987f3b6a1c948b28f05eb0d575cc
Leen27 <m13840918032@163.com>
2025-11-14 11:20:21 +0800

    feat(table-core): ts 编译环境

--- packages/table-core/vite.config.ts
+++ packages/table-core/vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-import dts from 'vite-plugin-dts'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    dts({
-      tsconfigPath: 'tsconfig.json',
-      exclude: ['src/**/*.spec.ts']
-    })
-  ],
+  plugins: [],
   build: {
     outDir: 'dist',
     lib: {
